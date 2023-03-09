fb0d80743513418257b9aefedb38979b2dcbe324
Nnisarg Gada <gadannisarg@gmail.com>
2023-03-09 22:19:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 python -m pip install pyinstaller
 python -m pip install PySide6
+python -m pip install requests
+python -m pip install pyqtdarktheme
+python -m pip install pyqtdarktheme
 python -m PyInstaller Xenon-Browser.spec
 mkdir /usr/lib/Xenon-Browser
 cp $PWD/dist/Xenon-Browser /usr/lib/Xenon-Browser/Xenon-Browser
